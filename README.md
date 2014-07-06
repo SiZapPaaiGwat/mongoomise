60c6b48cb817dc016a6e19fde8721942b5da7fdf
simongfxu <xugaofan@gmail.com>
2014-07-06 20:45:05 +0800

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-mongoomise
-==========
+# mongoomise
 
-promisify mongoose via Bluebird, you can simple switch to other promise library
+------
+
+promisify mongoose via Bluebird.
+
+well, you can simply switch to any other promise library
+
+## mongoose basics
+
+understanding some mongoose basics to help you implement a better api
+
+> * models extends from mongoose.Model
+> * mongoose.models.ModelName equals mongoose.model('ModelName')
+> * ModelName.schema equals mongoose.modelSchemas.ModelName
+> * static methods should be extended on mongoose.Model with a dynamic context
+> * instance methods should be extended on mongoose.Model.prototype
+
+## Initialization
+
+```javascript
+var mongoose = require('mongoose')
+var mongoomise = require('mongoomise')
+mongoomise.promisifyAll(mongoose)
+ ```
+invoke `mongoomise.promisifyAll ` just one time, on your first mongoose require.
+then you can use the utilities in everywhere.
+
+## Usage
+
+```javascript
+//static method
+User.findOneAsync().then(function(user){
+	user.updated_time = new Date
+	//instance method
+	return user.saveAsync()
+}).then(function(results){
+	//user, number affected
+	console.log(results[0], results[1])
+}).error(function(err){
+	console.log(err)
+})
+```
+
+## Notes
+
+* Do i need to change my schema? No.
+* Does Schema.pre and Schema.post work as usual? Yes, some discuss [here](https://github.com/yamadapc/mongoose-bluebird-utils/issues/1)
+* Any side effects? Not sure, need more test case. 
+
---
+++ src/main.js
@@ -0,0 +1,78 @@
+var $ = require('lodash')
+var Promise = require('bluebird')
+
+/*
+ * ========
+ * promisify mongoose via Bluebird
+ * ========
+ *
+ * mongoose basics
+ * models extends from mongoose.Model
+ * mongoose.models.ModelName equals mongoose.model('ModelName')
+ * ModelName.schema equals mongoose.modelSchemas.ModelName
+ * static methods should be extended on mongoose.Model with a dynamic context
+ * instance methods should be extended on mongoose.Model.prototype
+ * */
+
+exports.promisifyAll = function(mongoose, suffix){
+	suffix = suffix || 'Async'
+	var  Model = mongoose.Model
+
+	// promisify static methods by appending a suffix, default `Async`
+	var modelStaticsList = [
+		'aggregate', 'count', 'create', 'distinct', 'ensureIndexes',
+		'find', 'findById', 'findByIdAndRemove', 'findByIdAndUpdate',
+		'findOne', 'findOneAndRemove', 'findOneAndUpdate',
+		'geoNear', 'geoSearch', 'mapReduce', 'populate', 'remove', 'update'
+	]
+	$.each(modelStaticsList, function(method){
+		Model[method + suffix] = function(){
+			// using this to ref the target child class, do not use Model
+			return Promise.promisify(Model[method]).apply(this, arguments)
+		}
+	})
+
+	// promisify exposed mongoose classes instance methods
+	var instanceSource = {
+		Query: [
+			'count', 'distinct', 'exec', 'find', 'findOne', 'findOneAndRemove',
+			'findOneAndUpdate', 'remove', 'update'
+		],
+		Model: [
+			//from Document
+			'update', 'validate', 'populate',
+			'remove', 'save'
+		]
+	}
+	$.forIn(instanceSource, function(methods, className){
+		var cls = mongoose[className], prototype = cls.prototype
+		var isDocument = className === 'Model'
+
+		$.each(methods, function(i){
+			var methodName = i + suffix
+			prototype[methodName] = function(){
+				/*
+				 * mongoose internal use hooks to add pre and post handlers,
+				 * you can not use Promise.promisify directly
+				 * but the traditional way always works.
+				 * */
+				if(isDocument) {
+					var doc = this, args = $.toArray(arguments)
+					return new Promise(function(resolve, reject){
+						args.push(function(err){
+							if(err) return reject(err)
+							var receivedArgs = $.toArray(arguments)
+							receivedArgs.shift()
+							//send the arguments as an array
+							resolve(receivedArgs)
+						})
+						//magic invocation
+						doc[i].apply(doc, args)
+					})
+				}
+				//for simple class
+				return Promise.promisify(prototype[i]).apply(this, arguments)
+			}
+		})
+	})
+}
